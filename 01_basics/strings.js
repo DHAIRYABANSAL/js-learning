6a3c7edb0bb7cfe79407da251e5ffce81f4a71cb
Dhairya Bansal <dhairyabansal@Dhairyas-MacBook-Pro.local>
2024-08-17 13:05:20 +0530

    add strings.js

---
+++ 01_basics/strings.js
@@ -0,0 +1,30 @@
+// const name = 'dhairya'
+
+// const repoCount = 20
+
+// // normal concatination
+// console.log(name + repoCount + ' Value');
+
+
+// //string interpolation
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// const myName = new String('dhairya ')
+// console.log(typeof myName);
+
+
+const newName = 'Dhairya Bansal'
+
+console.log(newName.length);
+console.log(newName.charAt(2));
+console.log(newName.indexOf('a'));
+console.log(newName.slice(-8,8));
+console.log(newName.substring(2,4));
+
+console.log(newName.replace('a','b'));
+console.log(newName.replaceAll('a','b'));
+
+
+console.log(newName.split(' ').join(' '));
