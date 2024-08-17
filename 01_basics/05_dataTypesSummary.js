5ce48d1ff366f270bd4470f69a62bf9e2060e666
Dhairya Bansal <dhairyabansal@Dhairyas-MacBook-Pro.local>
2024-08-17 12:38:25 +0530

    dd datatype summary

--- 01_basics/05_dataTypesSummary.js
+++ 01_basics/05_dataTypesSummary.js
@@ -18,13 +18,49 @@
 //3. functions 
 
 
-const id = Symbol(123)
-const another = Symbol(123)
-console.log(id === another);
+// const id = Symbol(123)
+// const another = Symbol(123)
+// console.log(id === another);
 
-const str1 = Symbol('dhairya')
-const str2 = Symbol('dhairya')
-console.log(str1 === str2);
+// const str1 = Symbol('dhairya')
+// const str2 = Symbol('dhairya')
+// console.log(str1 === str2);
 
 
-console.log(typeof id);
+// console.log(typeof id);
+
+
+// +++++++++++++++++++++ Memory ++++++++++++++++++++++++++++
+
+// Primitive Types => Stack Memory 
+// Non - Primitive => Heap Memory
+
+
+// let myName = 'dhairya'
+// let anotherName = myName 
+
+
+// console.log(anotherName);
+
+// anotherName = 'dhairya bansal'
+
+// console.log(anotherName);
+// console.log(myName);
+
+
+let user = {
+    name : 'dhairya',
+    email : '@gmail.com',
+    id : 2
+}
+
+let user2 = user
+
+console.log(user2);
+
+user2.name = 'dhairya bansal'
+
+console.log(user2);
+console.log(user);
+
+console.log(user === user2);
