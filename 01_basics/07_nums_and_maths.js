878d68866ea6903d351ccfed5ad7428529ed79a3
Dhairya Bansal <dhairyabansal@Dhairyas-MacBook-Pro.local>
2024-08-20 09:00:06 +0530

    add 08_numns_and_maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,29 @@
+// let score = 100
+
+// console.log(score.toString());
+
+
+// let number = 108.998
+
+// console.log(number.toFixed(2));
+
+// let hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++  Maths +++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log((Math.abs(-4)));
+// console.log((Math.round(4.44)));
+// console.log((Math.ceil(4.44)));
+// console.log((Math.floor(4.44)));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))  + min)
